c4e9cf5d18b7922c92e4ebda3bc84b43d78075b2
Dylon Rodriguez <drodriguez@student.neumont.edu>
2017-02-13 11:25:52 -0700

    Init
    Create project folders and main method/class.

---
+++ CSC180_FinalProject/src/bruton_rodriguez/Browser_main.java
@@ -0,0 +1,16 @@
+package bruton_rodriguez;
+
+/**
+ * Created by dylonrodriguez on 2/13/17.
+ */
+public class Browser_main {
+    public static void main(String[] args) {
+        System.out.println("Browser_main.main");
+    }
+
+    /* Possible Names //
+
+    flagellum (name given by Brad Gustafson)
+
+     */
+}
